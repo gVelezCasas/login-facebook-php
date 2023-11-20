1f4ba1e4f9c58a3828b10b6ad77a7687e6230aef
angulini <seenfria@hotmail.com>
2023-11-20 18:49:04 +0100

    change of the library to a  diferent one from the sdk of facebook

--- index.php
+++ index.php
@@ -1,50 +1,101 @@
 <?php
+session_start();
+
 require_once './vendor/autoload.php';
-
-use Instagram\FacebookLogin\FacebookLogin;
-
-$config = array( // instantiation config params
-    'app_id' => '<FB_APP_ID>', // facebook app id
-    'app_secret' => '<FB_APP_SECRET>', // facebook app secret
-);
-
-// uri facebook will send the user to after they login
-$redirectUri = 'https://path/to/fb/login/redirect.php';
-
-$permissions = array( // permissions to request from the user
-    'instagram_basic',
-    'instagram_content_publish', 
-    'instagram_manage_insights', 
-    'instagram_manage_comments',
-    'pages_show_list', 
-    'ads_management', 
-    'business_management', 
-    'pages_read_engagement'
-);
-
-// instantiate new facebook login
-$facebookLogin = new FacebookLogin( $config );
-// display login dialog link
-echo '<a href="' . $facebookLogin->getLoginDialogUrl( $redirectUri, $permissions ) . '">' .
-    'Log in with Facebook' .
-'</a>';
-
-?>
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); 
+$dotenv->load();
+$fb = new Facebook\Facebook([
+    'app_id' => $_ENV['FB_APP_ID'],
+    'app_secret' => $_ENV['FB_APP_SECRET'],
+    'default_graph_version' => 'v18.0',
+    ]);
+  
+  $helper = $fb->getRedirectLoginHelper();
+  
+  $permissions = ['email','public_profile']; // Optional permissions
+  $loginUrl = $helper->getLoginUrl('https://takeaway-dev.es/login-ig/', $permissions);
+?> 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"> 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Instagram</title>
-    <link href="./css/app.css">
+    <link href="./css/app.css" rel="stylesheet">
     <script src="./js/app.js"></script>
-</head>
+</head> 
 <body>
-    <main class="container">
+    <main class="container"> 
         <div class="grid-item">
-            <h1>Login Instagram</h1>
-            <p>Click the button below to login to Instagram</p>
-            <a href="<?php echo $facebookLogin->getLoginDialogUrl( $redirectUri, $permissions ); ?>" class="btn btn-primary">Log in with Facebook</a>
+            <?php 
+                if(isset($_GET['code'])){ 
+                    try {
+                        $accessToken = $helper->getAccessToken();
+                      } catch(Facebook\Exceptions\FacebookResponseException $e) {
+                        // When Graph returns an error
+                        echo 'Graph returned an error: ' . $e->getMessage();
+                        exit;
+                      } catch(Facebook\Exceptions\FacebookSDKException $e) {
+                        // When validation fails or other local issues
+                        echo 'Facebook SDK returned an error: ' . $e->getMessage();
+                        exit;
+                      }
+                      
+                      if (! isset($accessToken)) {
+                        if ($helper->getError()) {
+                          header('HTTP/1.0 401 Unauthorized');
+                          echo "Error: " . $helper->getError() . "\n";
+                          echo "Error Code: " . $helper->getErrorCode() . "\n";
+                          echo "Error Reason: " . $helper->getErrorReason() . "\n";
+                          echo "Error Description: " . $helper->getErrorDescription() . "\n";
+                        } else {
+                          header('HTTP/1.0 400 Bad Request');
+                          echo 'Bad request';
+                        }
+                        exit;
+                      } 
+                      
+                      // Logged in
+                      echo '<h3>Access Token</h3>';
+                      var_dump($accessToken->getValue());
+                      
+                      // The OAuth 2.0 client handler helps us manage access tokens
+                      $oAuth2Client = $fb->getOAuth2Client();
+                      
+                      // Get the access token metadata from /debug_token
+                      $tokenMetadata = $oAuth2Client->debugToken($accessToken);
+                      echo '<h3>Metadata</h3>';
+                      var_dump($tokenMetadata);
+                      
+                      // Validation (these will throw FacebookSDKException's when they fail)
+                      $tokenMetadata->validateAppId($config['app_id']);
+                      // If you know the user ID this access token belongs to, you can validate it here
+                      //$tokenMetadata->validateUserId('123');
+                      $tokenMetadata->validateExpiration();
+                      
+                      if (! $accessToken->isLongLived()) {
+                        // Exchanges a short-lived access token for a long-lived one
+                        try {
+                          $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
+                        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+                          echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
+                          exit;
+                        }
+                      
+                        echo '<h3>Long-lived</h3>';
+                        var_dump($accessToken->getValue());
+                      }
+                      
+                      $_SESSION['fb_access_token'] = (string) $accessToken;
+                }else{ ?>
+                    <div class="info">
+                        <h1>Login Instagram</h1>
+                        <p>Click the button below to login to Instagram</p>
+                        <a href="<?php echo $loginUrl; ?>" class="btn btn-primary">Log in with Facebook</a>
+                    </div>
+                <?php }
+            ?>
+            
         </div>
     </main>
 </body>
