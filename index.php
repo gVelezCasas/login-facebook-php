4f794983a35e52aab55a6cf7094b2e2c51352e4a
angulini <seenfria@hotmail.com>
2023-11-21 18:57:58 +0100

    final solution to get the login to facebook

--- index.php
+++ index.php
@@ -1,19 +1,45 @@
 <?php
 session_start();
-
 require_once './vendor/autoload.php';
+use Instagram\FacebookLogin\FacebookLogin;
+use Instagram\AccessToken\AccessToken;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__); 
 $dotenv->load();
+
+//* config facebook login with facebook 3 party library *//
+
+$config = array( // instantiation config params
+  'app_id' => $_ENV['FB_APP_ID'], // facebook app id
+  'app_secret' => $_ENV['FB_APP_SECRET'], // facebook app secret
+  'graph_version' => $_ENV['GRAPH_VERSION'], // default is v2.10
+);
+$redirect_uri = 'https://takeaway-dev.es/login-facebook/';// facebook redirect uri
+
+$permissions = array( // permissions to request from the user
+  'public_profile',
+  'email',
+);
+// instantiate new facebook login
+$facebookLogin = new FacebookLogin( $config );
+
+// instantiate our access token class
+$accessToken = new AccessToken( $config );
+
+//* config facebook login with facebook 3 party library *//
+//* config facebook login with facebook sdk wrapper *//
 $fb = new Facebook\Facebook([
     'app_id' => $_ENV['FB_APP_ID'],
     'app_secret' => $_ENV['FB_APP_SECRET'],
     'default_graph_version' => 'v18.0',
     ]);
   
-  $helper = $fb->getRedirectLoginHelper();
+$helper = $fb->getRedirectLoginHelper();
   
-  $permissions = ['email','public_profile']; // Optional permissions
-  $loginUrl = $helper->getLoginUrl('https://takeaway-dev.es/login-ig/', $permissions);
+  // $permissions = ['email']; // Optional permissions
+  // if(!isset($_GET['code'])){
+  //   $loginUrl = $helper->getLoginUrl('https://localhost/login-facebook/', $permissions);
+  // }
+//* config facebook login with facebook sdk wrapper *//
 ?> 
 <!DOCTYPE html>
 <html lang="en"> 
@@ -28,72 +54,43 @@ $fb = new Facebook\Facebook([
     <main class="container"> 
         <div class="grid-item">
             <?php 
-                if(isset($_GET['code'])){ 
-                    try {
-                        $accessToken = $helper->getAccessToken();
-                      } catch(Facebook\Exceptions\FacebookResponseException $e) {
-                        // When Graph returns an error
-                        echo 'Graph returned an error: ' . $e->getMessage();
-                        exit;
-                      } catch(Facebook\Exceptions\FacebookSDKException $e) {
-                        // When validation fails or other local issues
-                        echo 'Facebook SDK returned an error: ' . $e->getMessage();
-                        exit;
-                      }
+            
+                if(isset($_GET['code']) &&  !isset($_SESSION['fb_access_token'])){ 
+                    $newToken = $accessToken->getAccessTokenFromCode( $_GET['code'], $redirect_uri );
+                    if ( !$accessToken->isLongLived() ) { // check if our access token is short lived (expires in hours)
+                      // exchange the short lived token for a long lived token which last about 60 days
+                      echo 'entra en el if no es long lived';
+                      $newToken = $accessToken->getLongLivedAccessToken( $newToken['access_token'] );
+                      $_SESSION['fb_access_token'] = (string) $newToken;
+                    }
+                    $_SESSION['fb_access_token'] = (string) $newToken['access_token'];
                       
-                      if (! isset($accessToken)) {
-                        if ($helper->getError()) {
-                          header('HTTP/1.0 401 Unauthorized');
-                          echo "Error: " . $helper->getError() . "\n";
-                          echo "Error Code: " . $helper->getErrorCode() . "\n";
-                          echo "Error Reason: " . $helper->getErrorReason() . "\n";
-                          echo "Error Description: " . $helper->getErrorDescription() . "\n";
-                        } else {
-                          header('HTTP/1.0 400 Bad Request');
-                          echo 'Bad request';
-                        }
-                        exit;
-                      } 
-                      
-                      // Logged in
-                      echo '<h3>Access Token</h3>';
-                      var_dump($accessToken->getValue());
-                      
-                      // The OAuth 2.0 client handler helps us manage access tokens
-                      $oAuth2Client = $fb->getOAuth2Client();
-                      
-                      // Get the access token metadata from /debug_token
-                      $tokenMetadata = $oAuth2Client->debugToken($accessToken);
-                      echo '<h3>Metadata</h3>';
-                      var_dump($tokenMetadata);
-                      
-                      // Validation (these will throw FacebookSDKException's when they fail)
-                      $tokenMetadata->validateAppId($config['app_id']);
-                      // If you know the user ID this access token belongs to, you can validate it here
-                      //$tokenMetadata->validateUserId('123');
-                      $tokenMetadata->validateExpiration();
-                      
-                      if (! $accessToken->isLongLived()) {
-                        // Exchanges a short-lived access token for a long-lived one
-                        try {
-                          $accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-                        } catch (Facebook\Exceptions\FacebookSDKException $e) {
-                          echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
-                          exit;
-                        }
-                      
-                        echo '<h3>Long-lived</h3>';
-                        var_dump($accessToken->getValue());
-                      }
-                      
-                      $_SESSION['fb_access_token'] = (string) $accessToken;
-                }else{ ?>
+                }else if(!isset($_SESSION['fb_access_token']) && !isset($_GET['code'])){ ?>
                     <div class="info">
                         <h1>Login Instagram</h1>
                         <p>Click the button below to login to Instagram</p>
-                        <a href="<?php echo $loginUrl; ?>" class="btn btn-primary">Log in with Facebook</a>
+                        <a href="<?php echo $facebookLogin->getLoginDialogUrl( $redirect_uri, $permissions ); ?>" class="btn btn-primary">Log in with Facebook</a>
                     </div>
                 <?php }
+                if(isset($_SESSION['fb_access_token'])){
+                  try {
+                    // Get the \Facebook\GraphNodes\GraphUser object for the current user.
+                    // If you provided a 'default_access_token', the '{access-token}' is optional.
+                    $response = $fb->get('/me?fields=email,name,first_name,picture,middle_name', $_SESSION['fb_access_token']);
+                  } catch(\Facebook\Exceptions\FacebookResponseException $e) {
+                    // When Graph returns an error
+                    echo 'Graph returned an error: ' . $e->getMessage();
+                    exit;
+                  } catch(\Facebook\Exceptions\FacebookSDKException $e) {
+                    // When validation fails or other local issues
+                    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+                    exit;
+                  }
+                  
+                  $me = $response->getGraphUser();
+                  echo 'Logged in as ' . $me->getEmail().$me->getName().$me->getFirstName().$me->getMiddleName();
+                  echo '<img src="'.$me->getPicture()->getUrl().'"/>';
+                }
             ?>
             
         </div>
